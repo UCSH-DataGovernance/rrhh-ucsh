88994388412541dc0e781e2a94626b0c6136361a
UCSH-DataGovernance <141373331+UCSH-DataGovernance@users.noreply.github.com>
2025-03-27 12:19:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 # Cargar datos
 @st.cache_data
 def load_data():
-    df = pd.read_csv("data/Resultados2024.csv", delimiter=";", encoding="ISO-8859-1")
+    df = pd.read_csv("data/Resultados2024.csv", delimiter=";", encoding="utf-8")
     df.columns = df.columns.str.strip().str.replace("\u00a0", " ").str.replace("\ufeff", "")
     
     columnas_requeridas = ["Nota Final Evaluación", "Ponderación Rol Evaluación"]
